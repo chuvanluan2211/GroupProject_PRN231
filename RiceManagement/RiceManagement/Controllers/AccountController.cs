ce58b17a0a43b921ace1be6d3319b1699c25c75b
HaiNam001 <hainamtran2k1@gmail.com>
2023-08-21 23:48:44 +0700

    LoginController

---
+++ RiceManagement/RiceManagement/Controllers/AccountController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using RiceManagement.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace RiceManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly ProjectBl5Context _context;
+        private readonly AppSettings _appSettings;
+        public AccountController(ProjectBl5Context context,IOptionsMonitor<AppSettings> optionsMonitor) {
+            _context= context;
+            _appSettings = optionsMonitor.CurrentValue;
+        }
+        [HttpPost]
+        public IActionResult Login(UserViewModel userViewModel)
+        {
+            var user = _context.Users.SingleOrDefault(u=>u.Email== userViewModel.Email && u.Password == userViewModel.Password);
+            if(user== null)
+            {
+                return Ok(new APIResponse
+                {
+                    Success = false,
+                    Message = "Invalid"
+                });
+            }
+            //get token
+            return Ok(new APIResponse
+            {
+                Success = true,
+                Message = "Successfull",
+                Data = GenerateToken(user)
+            });
+        }
+
+        private object GenerateToken(User user)
+        {
+            var jwtTokenHandler = new JwtSecurityTokenHandler();
+            var secretKeyBytes = Encoding.UTF8.GetBytes(_appSettings.SecretKey);
+            var tokenDescription = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Email,user.Email),
+                    new Claim("user_id",user.UserId.ToString()),
+                    //role
+                    new Claim("TokenId",Guid.NewGuid().ToString()),
+                }),
+                Expires = DateTime.UtcNow.AddMinutes(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(secretKeyBytes),
+                SecurityAlgorithms.HmacSha512Signature)
+            };
+            var token = jwtTokenHandler.CreateToken(tokenDescription);
+            return jwtTokenHandler.WriteToken(token);
+        }
+    }
+}
--- RiceManagement/RiceManagement/Program.cs
+++ RiceManagement/RiceManagement/Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using RiceManagement.Models;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,27 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ProjectBl5Context>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("StringDB"));
+});
+builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
+var secretKey = builder.Configuration["AppSettings:SecretKey"];
+var secretKeyBytes = Encoding.UTF8.GetBytes(secretKey);
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+{
+    opt.TokenValidationParameters = new TokenValidationParameters
+    {
+        //tu cap token
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        //ky vao token
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(secretKeyBytes),
+        ClockSkew = TimeSpan.Zero
+    };
+});
+builder.Services.AddAutoMapper(typeof(Program));
 
 var app = builder.Build();
 
@@ -17,6 +44,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
 
 app.UseAuthorization();
 
